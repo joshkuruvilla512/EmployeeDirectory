eead87fc54ae9bd4a6f81931fd2cf7f4d2cb44f5
Joshua Kuruvilla <joshkuruvilla512@gmail.com>
2020-02-24 09:26:43 -0600

    Setup table. Just need to put the correct information in and make a README.

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <div>
+            Employee Directory by Joshua Kuruvilla
+        </div>
+    )
+}
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+    return (
+        <div>
+            Employee Directory
+        </div>
+    )
+}
+
+export default Header;
